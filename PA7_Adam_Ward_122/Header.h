7f17bf85dead8419430daa0aa822849134137971
Bio <144750331+Bioticcc@users.noreply.github.com>
2024-08-01 09:30:39 -1000

    just the beggining baby
    W

---
+++ PA7_Adam_Ward_122/Header.h
@@ -0,0 +1,32 @@
+/*
+Author: Adam Ward
+Date: 8/1/2024
+Description: Wacky attendance tracker thang idk
+*/
+
+
+#pragma once
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>  
+#include <ostream>
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+#include <fstream>
+#include <sstream>
+
+using std::getline;
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::cin;
+using std::string;
+
+
+#endif 
